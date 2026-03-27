a2aa79bcd88be880f38a0702481894ee2fe1e25f
Giselle Martinez <al25120171@utcj.edu.mx>
2026-03-27 04:54:44 -0600

    Creación de middleware y rutas protegidas

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,12 +1,23 @@
 const express = require('express')
 const router = express.Router()
+
 const User = require('../models/userModel')
+const verifyToken = require('../middlewares/authMiddleware')
 
 router.get('/test-user', async (req, res) => {
 
   const user = await User.findByEmail('test@nexum.com')
 
   res.json(user)
+
+})
+
+router.get('/profile', verifyToken, (req, res) => {
+
+  res.json({
+    message: "Perfil del usuario autenticado",
+    user: req.user
+  })
 
 })
 
